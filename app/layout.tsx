daad38622d0f0b82e0b132732abb37091a0e4088
v8v88v8v88 <v8v88v8v88@protonmail.com>
2024-09-27 08:52:37 +0530

    changed the dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '/home/v8v88v8v88/skibidi-translator/app/globals.css'
+import './app/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
